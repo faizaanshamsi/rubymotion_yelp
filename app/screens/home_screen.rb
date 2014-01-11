4c8d662ad5b44d0dba268eb97df4249da5f88b05
faizaanshamsi <faizaanshamsi@gmail.com>
2014-01-11 16:15:58 -0500

    external links functional

--- app/screens/home_screen.rb
+++ app/screens/home_screen.rb
@@ -18,15 +18,14 @@ class HomeScreen < PM::TableScreen
   end
 
   def open_link(args)
-    p args[:cell][:title]
-    #put redirect to yelp link here
+    args[:cell][:url].nsurl.open
   end
 
   def create_data(category, location)
     BW::HTTP.get("http://api.yelp.com/business_review_search?ywsid=Smbd1bKAld_JSiOJ_naB0A&term=#{category}&location=#{location}") do |response|
       result_data = BW::JSON.parse(response.body)
       result_data["businesses"].map do |business|
-        @results << { title: business["name"], action: :open_link }
+        @results << { title: business["name"], url: business["mobile_url"], action: :open_link }
         update_table_data
       end
     end
