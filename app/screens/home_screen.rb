4d95137bd37d152ff9734c42e8b19b66fb13dc98
Mark Rickert <mjar81@gmail.com>
2014-01-14 15:20:52 -0500

    Make the search yelp method have a more relevant name.

--- app/screens/home_screen.rb
+++ app/screens/home_screen.rb
@@ -12,22 +12,25 @@ class HomeScreen < PM::TableScreen
     @results ||= [{
       title: "Loading...",
     }]
+  end
 
     category = NSUserDefaults['category']
     location = NSUserDefaults['location']
 
     create_data(category, location)
+  def on_appear
+    search_yelp
   end
 
   def open_link(args)
     args[:cell][:url].nsurl.open
   end
 
-  def create_data(category, location)
     BW::HTTP.get("http://api.yelp.com/business_review_search?ywsid=Smbd1bKAld_JSiOJ_naB0A&term=#{category}&location=#{location}") do |response|
       result_data = BW::JSON.parse(response.body)
       result_data["businesses"].map do |business|
         @results << { title: business["name"], url: business["mobile_url"], action: :open_link }
+  def search_yelp
       end
       update_table_data
     end
